ff69c47ef11f558c09bad378000a82aa4412d4d5
Ian Smith <ianhenrysmith@gmail.com>
2015-12-31 16:03:15 -0700

    ...

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+Rails.application.configure do
+  config.log_level = :debug
+end
+
 # Initialize the Rails application.
 Rails.application.initialize!
